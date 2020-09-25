f1ab6aa9245e3c17d1fc47b58cbe5adf6e55d1a0
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2020-09-25 17:06:02 +0800

    Update 279_Perfect_Squares.cpp

--- cpp/279_Perfect_Squares.cpp
+++ cpp/279_Perfect_Squares.cpp
@@ -4,44 +4,46 @@ Basic Idea: BFS, tree
 1. Regarding given value n as root, minus all squares which is less than n;
 2. After substration, if new value less than 0, back to queue; if new value equal to 0, return steps; if new value exists in visited set, continue to next new value.
 And then the rest of new values push into queue and visited set. Loop
+3. If n is prime, return n;
 */
 
 class Solution {
-    // 获取所有小于n的平方数
-    vector<int> getSquare(int n){
-        vector<int> res;
-        for(int i=1; i*i<=n; i++){
-            res.push_back(i*i);
+    vector<int> squareList(int n){
+        vector<int> sl;
+        for(int i=0; pow(i,2)<=n; i++){
+            sl.push_back(pow(i,2));
         }
-        return res;
+        return sl;
     }
 public:
     int numSquares(int n) {
-        vector<int> squares = getSquare(n);
+        vector<int> squarelist = squareList(n);
         unordered_set<int> visited;
         queue<int> q;
-        
+
         q.push(n);
-        int steps = 1;
         visited.insert(n);
+
+        int steps = 1;
 
         while(!q.empty()){
             int size = q.size();
+
             for(int i=0; i<size; i++){
-                int cur = q.front();
+                int temp = q.front();
                 q.pop();
-                
-                for(int num: squares){
-                    int next = cur - num;
+
+                for(int square: squarelist){
+                    int next = temp - square;
                     if(next < 0) break;
                     if(next == 0) return steps;
                     if(visited.count(next)) continue;
-
-                    visited.insert(next);
+                    
                     q.push(next);
+                    visited.insert(next);
                 }
             }
-            steps++;
+            steps++;       
         }
         return n;
     }
