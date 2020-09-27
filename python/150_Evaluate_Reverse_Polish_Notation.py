1864bb861e43fb94641594e3809a401e63871dbe
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2020-09-27 10:38:39 +0800

    Create 150_Evaluate_Reverse_Polish_Notation.py

---
+++ python/150_Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,26 @@
+'''
+date: 200927
+idea: stack to store number
+- when encountering a number, push it to the stack
+- when encounteringn a operator, take out the top two numbers of the stack for calculation, and push the result into the stack
+'''
+
+class Solution:
+    def evalRPN(self, tokens) -> int:
+        stack = list()
+        oper = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '/': operator.truediv,
+        }
+        for token in tokens:
+            if token not in oper:
+                stack.append(int(token))
+            else:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                temp = int(oper[token](op1, op2))
+                stack.append(temp)
+
+        return stack[-1]
