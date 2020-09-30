bf151135fc67aa3cb958786c267da06e505d8339
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2020-09-30 11:17:25 +0800

    Update 200_Number_of_Islands.cpp

--- cpp/200_Number_of_Islands.cpp
+++ cpp/200_Number_of_Islands.cpp
@@ -4,6 +4,8 @@ idea:
 1. DFS: recursion to search all of the neighbors which is '1'
 - write dfs function to set all the '1' neighbors of (grid[r][c]==1) to '0'. 
 - traverse all elements by dfs. increase island_nums if there is a '1'.
+
+S2. BFS: use queue to search all neighbors of one element and then to search other element neighbors.
 */
 
 class Solution {
@@ -35,5 +37,52 @@ public:
             }
         }
         return island_nums;
+    }
+};
+
+class Solution2 {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int nr = grid.size();
+        if(!nr) return 0;
+        int nc = grid[0].size();
+
+        int islands_num = 0;
+
+        for(int r=0; r<nr; r++){
+            for(int c=0; c<nc; c++){
+                if(grid[r][c] == '1'){
+                    islands_num++;
+                    grid[r][c] = '0';
+                    queue<pair<int, int>> neighbors;
+                    neighbors.push({r, c});
+
+                    while(!neighbors.empty()){
+                        auto rc = neighbors.front();
+                        neighbors.pop();
+                        int row = rc.first;
+                        int col = rc.second;
+
+                        if(row-1>=0 && grid[row-1][col] == '1'){
+                            grid[row-1][col] = '0';
+                            neighbors.push({row-1, col});
+                        } 
+                        if(row+1<nr && grid[row+1][col] == '1'){
+                            grid[row+1][col] = '0';
+                            neighbors.push({row+1, col});
+                        }
+                        if(col-1>=0 && grid[row][col-1] == '1'){
+                            grid[row][col-1] = '0';
+                            neighbors.push({row, col-1});
+                        }
+                        if(col+1<nc && grid[row][col+1] == '1'){
+                            grid[row][col+1] = '0';
+                            neighbors.push({row, col+1});
+                        }
+                    }
+                }
+            }
+        }
+        return islands_num;
     }
 };
