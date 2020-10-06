2a2ab39d35a2c53481f123df3a642df4dacd3420
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2020-10-06 13:24:15 +0800

    Create 24_Swap_Nodes_in_Pairs.cpp

---
+++ cpp/24_Swap_Nodes_in_Pairs.cpp
@@ -0,0 +1,28 @@
+/*
+date: 201006
+idea: recursion
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* first_node = head;
+        ListNode* second_node = head->next;
+
+        first_node->next = swapPairs(head->next->next);
+        second_node->next = first_node;
+
+        return second_node;
+    }
+};
