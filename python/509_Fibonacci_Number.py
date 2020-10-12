7a1ad08a86a7a502bc74d0ca8c661dc5b512350e
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2020-10-12 19:11:46 +0800

    Update 509_Fibonacci_Number.py

--- python/509_Fibonacci_Number.py
+++ python/509_Fibonacci_Number.py
@@ -1,6 +1,6 @@
 '''
 date: 201011
-idea: recur
+idea: recur+hashmap (memerization to reduce repeated calculation)
 '''
 
 class Solution:
@@ -8,3 +8,20 @@ class Solution:
         if N==0: return 0
         if N==1: return 1
         return self.fib(N-2) + self.fib(N-1)
+    
+
+class Solution2:
+    def fib(self, N: int) -> int:
+        cache = {}
+        def mem_fib(N): 
+            if N in cache:
+                return cache[N]
+
+            if N < 2:
+                return N
+            else:
+                res = mem_fib(N-1) + mem_fib(N-2)
+                cache[N] = res
+                return res
+
+        return mem_fib(N)
