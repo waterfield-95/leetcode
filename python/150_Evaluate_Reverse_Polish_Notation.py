690b57975ef2dc7b3726e270bb6d953b00738fe6
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2021-04-01 10:33:18 +0800

    Update 150_Evaluate_Reverse_Polish_Notation.py

--- python/150_Evaluate_Reverse_Polish_Notation.py
+++ python/150_Evaluate_Reverse_Polish_Notation.py
@@ -1,5 +1,10 @@
 '''
 date: 200927
+
+What's Reverse Polish Notation?
+- the operator follows their operands: 先写数字，后面跟着操作符号
+- Advantage: removes the need for parentheses 
+
 idea: stack to store number
 - when encountering a number, push it to the stack
 - when encounteringn a operator, take out the top two numbers of the stack for calculation, and push the result into the stack
