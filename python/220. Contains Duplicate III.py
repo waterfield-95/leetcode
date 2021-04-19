915abff3e07dc453c9acc9ed0e66b553fc933df6
YUAN TIAN <45745526+Fieldwater@users.noreply.github.com>
2021-04-19 15:29:06 +0800

    Update 220. Contains Duplicate III.py

--- python/220. Contains Duplicate III.py
+++ python/220. Contains Duplicate III.py
@@ -3,10 +3,11 @@
 idea:
 1. bisection
   - traverse element, judge whether or not there are elements in the range of [num-t, num+t] in the list of [max(0, i-k), i]
+  - find idx which is larger than num-t in the sorted window.
+  - and then 
 """
 
-
-class Solution(object):
+class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
         from bisect import bisect_left, insort_left
         window = []
@@ -14,6 +15,7 @@ class Solution(object):
             idx = bisect_left(window, num-t)
             if idx < len(window) and window[idx] <= num+t:
                 return True
+
             insort_left(window, num)
             if i >= k:
                 window.remove(nums[i-k])
