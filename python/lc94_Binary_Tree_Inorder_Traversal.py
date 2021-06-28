80d91aef4a17ac0d0ea9255b97ba935faff9bf31
Yuan Tian <tianyuan@seaeverit.com>
2021-06-28 17:44:41 +0800

    lc94 stack method and comments

--- python/lc94_Binary_Tree_Inorder_Traversal.py
+++ python/lc94_Binary_Tree_Inorder_Traversal.py
@@ -1,9 +1,23 @@
+"""
+2021.6.28
+idea: traverse a tree in inorder method
+    - find the most left one and get its value, and then get upper level node value
+    finally get right one value. Notice special condition which is None node.
+
+1. DFS
+2. stack
+
+"""
+
+from typing import List
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def inorder(self, root, res):
         if not root: return
@@ -14,4 +28,22 @@ class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
         self.inorder(root, res)
+        return res
+
+# stack
+class Solution2:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        # terminal condition
+        while root or stack:
+            # deal with left node and current node
+            if root:
+                stack.append(root)
+                root = root.left
+            # deal with right one and get result value from stack
+            else:
+                cur = stack.pop()
+                res.append(cur.val)
+                root = cur.right
         return res
